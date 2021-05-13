46db54c6ebd59fdc43e778545f11c8dfa81f7bbe
Karol Moroz <k.j.moroz@gmail.com>
2021-05-13 23:40:58 +0800

    Implement making a base request

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,8 +1,36 @@
-use std::env;
+extern crate reqwest;
+extern crate url;
 
-fn get_token() -> Option<String> {
+use std::env;
+use std::io::{Error, ErrorKind, Result};
+use url::form_urlencoded;
+
+pub const BASE_URL: &str = "https://api.openweathermap.org/data/2.5/weather";
+
+fn get_token() -> Result<String> {
     match env::var("WEATHER_API_TOKEN") {
-        Ok(val) => Some(val),
-        _ => None,
+        Ok(val) => Ok(val),
+        Err(_) => Err(Error::new(ErrorKind::NotFound, "Could not fetch API token")),
     }
+}
+
+fn encode_url_for_city(city: &str) -> Result<String> {
+    let token = get_token()?;
+
+    let encoded: String = form_urlencoded::Serializer::new(String::new())
+        .append_pair("q", city)
+        .append_pair("appid", &token)
+        .finish();
+
+    let joined = format!("{}?{}", BASE_URL, encoded);
+
+    Ok(joined)
+}
+
+pub async fn get_weather_for_city(city: &str) -> Result<String> {
+    let url = encode_url_for_city(city)?;
+
+    let body = reqwest::get(url).await.unwrap().text().await.unwrap();
+
+    Ok(body)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 extern crate dotenv;
+extern crate futures;
 
 pub mod api;
 
 use dotenv::dotenv;
 use std::env;
+use tokio::runtime;
+
+fn print_usage() {
+    println!("Weather app. Usage: weather CITY")
+}
 
 fn main() {
     dotenv().ok();
 
-    for (key, value) in env::vars() {
-        println!("{}: {}", key, value);
+    let argc = env::args().len();
+
+    if argc == 1 {
+        print_usage();
+    } else {
+        let rt = runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        let city = env::args().nth(1).unwrap();
+        println!("Selected city: {}", city);
+        let future = async {
+            let response = match api::get_weather_for_city(&city).await {
+                Ok(weather) => weather,
+                Err(_) => "Failed to fetch weather.".to_string(),
+            };
+            println!("{}", response);
+        };
+        rt.block_on(future);
     }
 }
